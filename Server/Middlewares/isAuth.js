76c9820028e3f0c936c0db4f1e393c2d5602c55b
PranavThorat1432 <pranavthorat95@gmail.com>
2026-02-19 20:45:13 +0530

    isAuth Middleware

---
+++ Server/Middlewares/isAuth.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import { catchAsyncErrors } from './catchAsyncError.js';
+import ErrorHandler from './errorMiddlewares.js';
+import database from '../Config/db.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const isAuth = catchAsyncErrors(async (req, res, next) => {
+    const {token} = req.cookies;
+    if(!token) {
+        return next(new ErrorHandler('Please Login to access this resource.', 401));
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    const user = await database.query('SELECT * FROM users WHERE id = $1 LIMIT 1', [decoded.id]);
+
+    req.user = user.rows[0];
+    next();
+});
+
+
+export const authorizedRoles = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)) {
+            return next(new ErrorHandler(`Role: ${req.user.role} is not allowed to access this resource.`, 403));
+        }
+        next();
+    };
+};
