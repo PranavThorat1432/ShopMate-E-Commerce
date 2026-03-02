79945f8f4cd74185d47e30dd015568656b052b89
PranavThorat1432 <pranavthorat95@gmail.com>
2026-03-02 14:49:09 +0530

    Server (Main File)

---
+++ Server/server.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import app from './app.js';
+import {v2 as cloudinary} from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+    api_key: process.env.CLOUDINARY_CLIENT_API,
+    api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+});
+
+app.get('/', (req, res) => {
+	res.send('Server is Running!');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on PORT: http://localhost:${process.env.PORT}`);
+});
