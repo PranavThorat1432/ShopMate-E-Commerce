ef230351e72185041b1d7128405a34bddf60a77d
PranavThorat1432 <pranavthorat95@gmail.com>
2026-04-22 19:42:49 +0530

    Axios

---
+++ Dashboard (Admin)/src/lib/axios.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+    baseURL: import.meta.env.MODE === "development" 
+        ? import.meta.env.VITE_BACKEND_URL || "http://localhost:3000" 
+        : "/", 
+    withCredentials: true,
+});
