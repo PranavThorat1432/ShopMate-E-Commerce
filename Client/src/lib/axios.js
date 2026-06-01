a4348a3e106f103de928621426f90a8558c7a72d
PranavThorat1432 <pranavthorat95@gmail.com>
2026-06-01 15:55:38 +0530

    Update Axios Instance

--- Client/src/lib/axios.js
+++ Client/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" 
-        ? import.meta.env.VITE_BACKEND_URL || "http://localhost:3000" 
-        : "/", 
+    baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000",
     withCredentials: true,
 });
